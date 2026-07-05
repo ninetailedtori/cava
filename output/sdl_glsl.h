47c23f195210f8a70a8e0b18b69f030a2c32fc72
Toria <ninetailedtori@uwu.gal>
2026-07-05 22:48:06 +0100

    Fix lint.
    Signed-off-by: Toria <ninetailedtori@uwu.gal>

--- output/sdl_glsl.h
+++ output/sdl_glsl.h
@@ -8,16 +8,11 @@
 #include "../config.h"
 
 void init_sdl_glsl_window(int width, int height, int x, int y, int full_screen,
-                          const char * vertex_shader,
-                          const char * fragment_shader);
-GLuint create_gradient_texture(int gradient_count,
-                               char **gradient_color_strings);
-void init_sdl_glsl_surface(int *width,int *height,
-                           const char * fg_color_string,
-                           const char * bg_color_string, int bar_width,
-                           int bar_spacing, int gradient, int gradient_count,
-                           char **gradient_color_strings);
-int draw_sdl_glsl(int bars_count, const float bars[],
-                  const float previous_bars[], int frame_timer, int re_paint,
-                  int continuous_rendering);
+                          const char *vertex_shader, const char *fragment_shader);
+GLuint create_gradient_texture(int gradient_count, char **gradient_color_strings);
+void init_sdl_glsl_surface(int *width, int *height, const char *fg_color_string,
+                           const char *bg_color_string, int bar_width, int bar_spacing,
+                           int gradient, int gradient_count, char **gradient_color_strings);
+int draw_sdl_glsl(int bars_count, const float bars[], const float previous_bars[], int frame_timer,
+                  int re_paint, int continuous_rendering);
 void cleanup_sdl_glsl(void);
